cc68cba66d4de9ed313894747fff79e757de7d71
Lu Daven <daven.l@triplet-rd.com>
2021-12-15 14:24:44 +0900

    first commit local

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BaklavaVuePlugin } from "@baklavajs/plugin-renderer-vue";
+import "@baklavajs/plugin-renderer-vue/dist/styles.css";
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+Vue.use(BaklavaVuePlugin);
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
